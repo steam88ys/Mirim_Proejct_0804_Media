dbf2fd8642992c50be71186d838566c6500cdf56
steam88ys <yssteam88@gmail.com>
2022-08-04 09:46:27 +0900

    Media

---
+++ app/src/main/java/kr/hs/emirim/s2102/mirim_proejct_0804_media/MainActivity.java
@@ -0,0 +1,29 @@
+package kr.hs.emirim.s2102.mirim_proejct_0804_media;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mPlayer;
+    Switch switch1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPlayer = MediaPlayer.create(this, R.raw.music01);
+        switch1 = findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b) mPlayer.start();
+                else mPlayer.stop();
+            }
+        });
+    }
+}
